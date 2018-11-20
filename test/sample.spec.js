402237bdb2bd6bf5a3a0211184913bc14bad96b7
stepanchaparyan <schaparian@yahoo.com>
2018-11-21 01:09:26 +0400

    add cases for List.am, update startWT

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -26,8 +26,6 @@ describe('browser version', async () => {
 })
 
 describe('start', async () => {
-  var cookies
-
   beforeEach(async () => {
     await page.goto('https://app.webtrends-optimize.com/optimize/manage/assets')
     await page.waitForSelector('#emailInput')
@@ -37,24 +35,17 @@ describe('start', async () => {
     await page.waitFor(5000)
     await page.click('body > main > div > div > div > div:nth-child(2) > div > div.account-groups > div:nth-child(2) > div > a')
     await page.waitFor(15000)
-    cookies = await page.cookies()
     await page.screenshot({ path: 'screenshots/wtlogin.png' })
   })
   it('asset', async () => {  
-    const page2 = await browser.newPage()
-    await page2.setCookie(...cookies)
-    await page2.goto('https://app.webtrends-optimize.com/optimize/manage/assets')
-    await page2.setCookie(...cookies)
+    await page.goto('https://app.webtrends-optimize.com/optimize/manage/assets')
     await page.waitFor(20000)
     await page.click('body > main > div > div.app-content.row > div > div.container > div > section > div > div.tableBody > div:nth-child(2)')
     await page.waitFor(3000)
     await page.screenshot({ path: 'screenshots/wtAsset.png' })
   })
-  it('Space', async () => {  
-    const page2 = await browser.newPage()
-    await page2.setCookie(...cookies)
-    await page2.goto('https://app.webtrends-optimize.com/optimize/manage/assets')
-    await page2.setCookie(...cookies)
+  it('availableSpace', async () => {  
+    await page.goto('https://app.webtrends-optimize.com/optimize/manage/assets')
     await page.waitFor(20000)
     await page.click('body > main > div > div.app-content.row > div > div.container > div > div:nth-child(1) > div > div:nth-child(1) > div > div > div > span')
     await page.waitFor(3000)
@@ -62,51 +53,77 @@ describe('start', async () => {
   })
 })
 
-describe('loginOne', async () => {
-  let cookies
-  beforeEach(async () => {
-    await page.goto('https://github.com/github')
-    await page.waitForSelector('body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.d-flex.flex-justify-between.flex-items-center.flex-auto > div.d-flex.flex-items-center.px-0.text-center.text-left > a.HeaderMenu-link.no-underline.mr-3')
-    await page.click('body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.d-flex.flex-justify-between.flex-items-center.flex-auto > div.d-flex.flex-items-center.px-0.text-center.text-left > a.HeaderMenu-link.no-underline.mr-3')
-    await page.waitFor(4000)
-    await page.type('#login_field', CREDS.usernameS)
-    await page.type('#password', CREDS.passwordS)
-    await page.click('#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block')
-    await page.waitFor(5000)
-    cookies = await page.cookies();
-  })
-   it('GitLogin', async () => {  
-    const page2 = await browser.newPage();
-    await page2.setCookie(...cookies);
-    await page.goto('https://github.com/github')
-    await page.waitFor(9000)
-    await page.screenshot({ path: 'screenshots/gitlogin.png' })
-  })
-})  
-
-describe('list', async () => {
-  let cookies
+describe('list.am', async () => {
   beforeEach(async () => {
     await page.goto('https://www.list.am')
     await page.waitForSelector('#ma')
     await page.click('#ma')
-    await page.waitFor(1000)
+    await page.waitFor(2000)
     await page.type('#_idyour_email', CREDS.usernameS)
     await page.type('#_idpassword', CREDS.passwordS)
     await page.click('#loginaction__form_action0')
-    await page.waitFor(1000)
-    cookies = await page.cookies();
+    await page.waitFor(2000)
   })
-  it('GitLogin', async () => {  
-    const page2 = await browser.newPage();
-    await page2.setCookie(...cookies);
+  it('myPage', async () => {  
     await page.goto('https://www.list.am')
     await page.click('#ma')
-    await page.waitFor(3000)
-    await page.screenshot({ path: 'screenshots/listlogin.png' })
+    await page.waitFor(2000)
+    await page.screenshot({ path: 'screenshots/listMyPage.png' })
   })
-}) 
+  it('wantedItemsPage', async () => {  
+    await page.goto('https://www.list.am')
+    await page.click('#sa')
+    await page.waitFor(2000)
+    await page.screenshot({ path: 'screenshots/listMyWanted.png' })
+  })
+  it('searchPhone', async () => {  
+    await page.goto('https://www.list.am')
+    await page.hover('#menu > div.c > span:nth-child(4) > a')    
+    await page.click('#menu > div.c > span:nth-child(4) > div > span:nth-child(1) > div:nth-child(2) > a:nth-child(1)')
+    await page.waitFor(2000)
+    await page.screenshot({ path: 'screenshots/listPhone.png' })
+  })
+  it('changeLanguage', async () => {  
+    await page.goto('https://www.list.am')
+    await page.click('#lbar')
+    await page.waitFor(2000)
+    await page.click('#lmenu > a:nth-child(2)')
+    await page.waitFor(2000)
+    await page.screenshot({ path: 'screenshots/listLangBar.png' })
+    const text = await page.$eval('#ma', element => element.innerText);
+    expect(text).equal('My Account')
+  })
+  it('itemPriceCheck', async () => {  
+    await page.goto('https://www.list.am')
+    await page.click('#sa')
+    await page.waitFor(2000)
+    const price = await page.$eval('#contentr > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > div.p', element => element.innerText);
+    await page.screenshot({ path: 'screenshots/listPrice1.png' })
+    expect(price).equal('18,000 ֏')
+  })
+  it('itemPriceCheckByPixel', async () => {  
+    await page.goto('https://www.list.am')
+    await page.click('#sa')
+    await page.waitFor(2000)
+    await page.screenshot({ path: 'screenshots/listPrice2.png' })
 
+    const img1 = fs.createReadStream('screenshots/listPrice1.png').pipe(new PNG()).on('parsed', doneReading)
+    const img2 = fs.createReadStream('screenshots/listPrice2.png').pipe(new PNG()).on('parsed', doneReading)
+    let filesRead = 0
+    let numDiffPixel
+
+    function doneReading() {
+        if (++filesRead < 2) return
+        let diff = new PNG({width: img1.width, height: img1.height})
+        numDiffPixel = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {threshold: 0.1})
+        //console.log("pixel is: " + numDiffPixel)
+        diff.pack().pipe(fs.createWriteStream('screenshots/diff.png'))
+        expect(numDiffPixel).equal(0)
+        done()
+    }
+  }) 
+
+}) 
 
 afterEach(async () => {
   await browser.close()
