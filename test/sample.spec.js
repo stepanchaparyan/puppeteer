df9b7a647c34916485346fae58ac10a19c555ec9
stepanchaparyan <schaparian@yahoo.com>
2018-11-20 20:03:14 +0400

    update wtTestCases

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -11,7 +11,7 @@ const showUI = {headless: false}
 const showSlowMotion = {headless: false, slowMo: 300}
 
 beforeEach(async () => {
-  browser = await puppeteer.launch(showUI)
+  browser = await puppeteer.launch()
   page = await browser.newPage()
   await page.setViewport(viewport)
 })
@@ -26,17 +26,87 @@ describe('browser version', async () => {
 })
 
 describe('start', async () => {
-  it('loginWT', async () => {  
+  var cookies
+
+  beforeEach(async () => {
     await page.goto('https://app.webtrends-optimize.com/optimize/manage/assets')
     await page.waitForSelector('#emailInput')
     await page.type('#emailInput', CREDS.usernameA)
     await page.type('#passwordInput', CREDS.passwordA)
     await page.click('.submit-button')
+    await page.waitFor(5000)
+    await page.click('body > main > div > div > div > div:nth-child(2) > div > div.account-groups > div:nth-child(2) > div > a')
     await page.waitFor(15000)
+    cookies = await page.cookies()
     await page.screenshot({ path: 'screenshots/wtlogin.png' })
   })
-
+  it('asset', async () => {  
+    const page2 = await browser.newPage()
+    await page2.setCookie(...cookies)
+    await page2.goto('https://app.webtrends-optimize.com/optimize/manage/assets')
+    await page2.setCookie(...cookies)
+    await page.waitFor(20000)
+    await page.click('body > main > div > div.app-content.row > div > div.container > div > section > div > div.tableBody > div:nth-child(2)')
+    await page.waitFor(3000)
+    await page.screenshot({ path: 'screenshots/wtAsset.png' })
+  })
+  it('Space', async () => {  
+    const page2 = await browser.newPage()
+    await page2.setCookie(...cookies)
+    await page2.goto('https://app.webtrends-optimize.com/optimize/manage/assets')
+    await page2.setCookie(...cookies)
+    await page.waitFor(20000)
+    await page.click('body > main > div > div.app-content.row > div > div.container > div > div:nth-child(1) > div > div:nth-child(1) > div > div > div > span')
+    await page.waitFor(3000)
+    await page.screenshot({ path: 'screenshots/wtSpace.png' })
+  })
 })
+
+describe('loginOne', async () => {
+  let cookies
+  beforeEach(async () => {
+    await page.goto('https://github.com/github')
+    await page.waitForSelector('body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.d-flex.flex-justify-between.flex-items-center.flex-auto > div.d-flex.flex-items-center.px-0.text-center.text-left > a.HeaderMenu-link.no-underline.mr-3')
+    await page.click('body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.d-flex.flex-justify-between.flex-items-center.flex-auto > div.d-flex.flex-items-center.px-0.text-center.text-left > a.HeaderMenu-link.no-underline.mr-3')
+    await page.waitFor(4000)
+    await page.type('#login_field', CREDS.usernameS)
+    await page.type('#password', CREDS.passwordS)
+    await page.click('#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block')
+    await page.waitFor(5000)
+    cookies = await page.cookies();
+  })
+   it('GitLogin', async () => {  
+    const page2 = await browser.newPage();
+    await page2.setCookie(...cookies);
+    await page.goto('https://github.com/github')
+    await page.waitFor(9000)
+    await page.screenshot({ path: 'screenshots/gitlogin.png' })
+  })
+})  
+
+describe('list', async () => {
+  let cookies
+  beforeEach(async () => {
+    await page.goto('https://www.list.am')
+    await page.waitForSelector('#ma')
+    await page.click('#ma')
+    await page.waitFor(1000)
+    await page.type('#_idyour_email', CREDS.usernameS)
+    await page.type('#_idpassword', CREDS.passwordS)
+    await page.click('#loginaction__form_action0')
+    await page.waitFor(1000)
+    cookies = await page.cookies();
+  })
+  it('GitLogin', async () => {  
+    const page2 = await browser.newPage();
+    await page2.setCookie(...cookies);
+    await page.goto('https://www.list.am')
+    await page.click('#ma')
+    await page.waitFor(3000)
+    await page.screenshot({ path: 'screenshots/listlogin.png' })
+  })
+}) 
+
 
 afterEach(async () => {
   await browser.close()
