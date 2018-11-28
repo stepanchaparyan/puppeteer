b94014623021c5d083abeeff481f3f4e04dfdb7c
stepanchaparyan <schaparian@yahoo.com>
2018-11-28 19:32:46 +0400

    try drug n drop case

--- test/drugndroptest.spec.js
+++ test/drugndroptest.spec.js
@@ -3,10 +3,15 @@ import CREDS from "../creds"
 
 (async () => {
   const browser = await puppeteer.launch({
-    
+    headless: false
   })
 
+
   const page = await browser.newPage()
+  
+  await page.setViewport({width: 1200, height: 800, deviceScaleFactor: 1});
+
+
   await page.goto('https://app.webtrends-optimize.com/optimize/manage/segments')
   await page.waitFor(1000)
   await page.waitForSelector('#emailInput')
@@ -18,23 +23,34 @@ import CREDS from "../creds"
   await page.click('body > main > div > div.app-content.row > div > div > div:nth-child(1) > div > div > span.section-header__toolbar > button > svg')
   await page.click('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.col-xs-4 > section > div > ul > li:nth-child(4)')
   
-  const element = await page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.col-xs-4 > section > div > ul > li:nth-child(5)')
+  const element = await page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.col-xs-4 > section > div > ul > li:nth-child(2)')
+  const drugableArea = await page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.filter-sandbox.col-xs-8 > div > article')
   const box = await element.boundingBox()
+  const area = await drugableArea.boundingBox()
+
   const mouse = page.mouse
-  await mouse.move(box.x + box.width / 2, box.y + box.height / 2)
-  console.log(box.x)
-  console.log(box.width)
+  //await page.click('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.col-xs-4 > section > div > ul > li:nth-child(2)')
+
+  await mouse.move(box.x + box.width / 2, box.y + box.height / 2, {steps: 200})
+  console.log(box.x + box.width / 2)
+  console.log(box.y + box.height / 2)
 
   await mouse.down()  
-  await page.waitFor(2000)
-  await page.screenshot({path: 'screenshots/1.png', fullPage: true})
-  await mouse.move(100, 80, {steps: 100})
-  await page.screenshot({path: 'screenshots/2.png', fullPage: true})
-  await mouse.move(380, 240, {steps: 100})
-  await page.waitFor(1000)
+  //await page.screenshot({path: 'screenshots/1.png', fullPage: true})
+  //await mouse.move(area.x + area.width / 2, area.y + area.height / 2, {steps: 100})
+  //await page.screenshot({path: 'screenshots/2.png', fullPage: true})
+  await mouse.move(450, 240, {steps: 200})
   await mouse.up()
-  await page.waitFor(1000)
-  await page.screenshot({path: 'screenshots/3.png', fullPage: true})
+  await page.screenshot({path: 'screenshots/3.png'})
+
+  // await page.setViewport({width: 1200, height: 800, deviceScaleFactor: 1});
+  // await page.goto('https://www.google.de/maps/@52.4889552,13.4707445,1555m/data=!3m1!1e3')
+
+  // const mouse = page.mouse
+  // await mouse.move(500, 500)
+  // await mouse.down()
+  // await mouse.move(200, 200, {steps: 100})
+  // await mouse.up()
 
   browser.close()
 })()
