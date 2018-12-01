3e2d38559902ee648fa5c9f83b8dbdd78d330605
stepanchaparyan <schaparian@yahoo.com>
2018-12-01 17:27:07 +0400

    add cases for segments page

--- pageobjects/WTsegments.js
+++ pageobjects/WTsegments.js
@@ -89,15 +89,57 @@ export default class SegmentBuilder {
         return await this.page.$('body > main > div > div.app-content.row > div > div > div:nth-child(3) > div > div > div > table > tbody > tr:nth-child(1) > td.td--buttons > svg > path') !== null
     }
     
-    
-    
+    async goToAddSegmentPage() {
+        await this.page.click('body > main > div > div.app-content.row > div > div > div:nth-child(1) > div > div > span.section-header__toolbar > button > svg')
+        await this.page.screenshot({ path: 'screenshots/segmentLogin.png' })
+    }
+    async sectionCreateSegmentExist() {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-header.row > div > div:nth-child(1) > div > span') !== null
+    }
+    async segmentNameInputExist() {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-header.row > div > div:nth-child(1) > div > input') !== null
+    }    
+    async segmentDescriptionTitleExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-header.row > div > div:nth-child(2) > div > span:nth-child(1)') !== null
+    }
+    async segmentDescriptionInputExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-header.row > div > div:nth-child(2) > div > input') !== null
+    }
+    async segmentCreatedDateExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-header.row > div > div:nth-child(2) > div > span.text-label.flex--align-self-start > span > span') !== null
+    }
+    async dropAttributeHereFieldExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.filter-sandbox.col-xs-8 > div > article > span') !== null
+    }
+    async droppableSectionArrowExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.filter-sandbox.col-xs-8 > div > article > svg > path') !== null
+    }   
+    async cancelButtonExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-footer.row > button.primary.with-text.btn.btn-link') !== null
+    }
+    async saveButtonExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-footer.row > button.primary.with-text.btn.btn-primary') !== null
+    }
+
+    async categoryMySegmentsExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.col-xs-4 > section > div > ul > li.category.category-header') !== null
+    }
+    async categoryMySegmentsImageExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.col-xs-4 > section > div > ul > li.category.category-header > img') !== null
+    }
+    async categoryMySegmentsAngleRightExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.col-xs-4 > section > div > ul > li.category.category-header > svg') !== null
+    }
+    async categoryMySegmentsIcon1Exist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.col-xs-4 > section > div > ul > li:nth-child(1) > svg:nth-child(1) > path') !== null
+    }
+    async categoryMySegmentsIcon2Exist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.col-xs-4 > section > div > ul > li:nth-child(1) > svg:nth-child(2) > path') !== null
+    }
+    async categoryMySegmentsNameExist () {
+        return await this.page.$('body > main > div > div.app-content.row > div > div > div > div.segment-content.create.row > div.col-xs-4 > section > div > ul > li:nth-child(2) > span') !== null
+    }
     // async tableHeadIDExist () {
     //     return await this.page.$('') !== null
-    // }
-    // async tableHeadIDExist () {
-    //     return await this.page.$('') !== null
-    // }
-    // async tableHeadIDExist () {
-    //     return await this.page.$('') !== null
-    // }
+    // }   
 }
--- test/WTsegments.spec.js
+++ test/WTsegments.spec.js
@@ -17,35 +17,36 @@ beforeEach(async () => {
   await page.setViewport(viewport)
 })
 
-describe('elements exist', async () => {
+describe.skip(`first page's elements exist`, async () => {
   beforeEach(async () => {
     segmentBuilder = new SegmentBuilder(page)
     await segmentBuilder.open()
     await segmentBuilder.logIn()
   })
-  it('logoExist', async () => {
-    expect(await segmentBuilder.logoExist()).to.be.true; 
+  context("mainPart", async() => {
+    it('logoExist', async () => {
+      expect(await segmentBuilder.logoExist()).to.be.true; 
+    })
+    it('titleExist', async () => {
+      expect(await segmentBuilder.titleExist()).to.be.true; 
+    })
+    it('searchFieldExist', async () => {
+      expect(await segmentBuilder.searchFieldExist()).to.be.true; 
+    })
+    it('addSegmentButtonExist', async () => {
+      expect(await segmentBuilder.addSegmentButtonExist()).to.be.true; 
+    })
+    it('allSegmentsFilterExist', async () => {
+      expect(await segmentBuilder.allSegmentsFilterExist()).to.be.true; 
+    })
+    it('inUseFilterExist', async () => {
+      expect(await segmentBuilder.inUseFilterExist()).to.be.true; 
+    })
+    it('notInUseFilterExist', async () => {
+      expect(await segmentBuilder.notInUseFilterExist()).to.be.true; 
+    })
   })
-  it('titleExist', async () => {
-    expect(await segmentBuilder.titleExist()).to.be.true; 
-  })
-  it('searchFieldExist', async () => {
-    expect(await segmentBuilder.searchFieldExist()).to.be.true; 
-  })
-  it('addSegmentButtonExist', async () => {
-    expect(await segmentBuilder.addSegmentButtonExist()).to.be.true; 
-  })
-  it('allSegmentsFilterExist', async () => {
-    expect(await segmentBuilder.allSegmentsFilterExist()).to.be.true; 
-  })
-  it.skip('inUseFilterExist', async () => {
-    expect(await segmentBuilder.inUseFilterExist()).to.be.true; 
-  })
-  it.skip('notInUseFilterExist', async () => {
-    expect(await segmentBuilder.notInUseFilterExist()).to.be.true; 
-  })
-
-  context.skip("tableHeads", async() => {
+  context("tableHeads", async() => {
     it('tableHeadSegmentsExist', async () => {
       expect(await segmentBuilder.tableHeadSegmentsExist()).to.be.true; 
     })
@@ -62,7 +63,6 @@ describe('elements exist', async () => {
       expect(await segmentBuilder.tableHeadCreatedExist()).to.be.true; 
     })
   })
-
   context("tableRows", async() => {
     it('tableRowSegmentsExist', async () => {
       expect(await segmentBuilder.tableRowSegmentsExist()).to.be.true; 
@@ -79,26 +79,81 @@ describe('elements exist', async () => {
     it('tableRowCreatedExist', async () => {
       expect(await segmentBuilder.tableRowCreatedExist()).to.be.true; 
     })
-    context.only("tableRowIcons", async() => {
+    context("tableRowIcons", async() => {
       it('tableRowUpdateIconExist', async () => {
         expect(await segmentBuilder.tableRowUpdateIconExist()).to.be.true; 
       })
       it('tableRowDeleteIconExist', async () => {
         expect(await segmentBuilder.tableRowDeleteIconExist()).to.be.true; 
       })
-      it.only('tableRowHiddenIconExist', async () => {
+      it('tableRowHiddenIconExist', async () => {
         expect(await segmentBuilder.tableRowHiddenIconExist()).to.be.true; 
       })
     })
   })
+})
 
-  // it('searchFieldExist', async () => {
-  //   expect(await segmentBuilder.searchFieldExist()).to.be.true; 
+describe(`addSegment page's elements exist`, async () => {
+  beforeEach(async () => {
+    segmentBuilder = new SegmentBuilder(page)
+    await segmentBuilder.open()
+    await segmentBuilder.logIn()
+    await segmentBuilder.goToAddSegmentPage()
+  })
+  context.skip(`mainPart's elements`, async() => {
+    it('sectionCreateSegmentExist', async () => {
+      expect(await segmentBuilder.sectionCreateSegmentExist()).to.be.true; 
+    })
+    it('segmentNameInputExist', async () => {
+      expect(await segmentBuilder.segmentNameInputExist()).to.be.true; 
+    })
+    it('segmentDescriptionTitleExist', async () => {
+      expect(await segmentBuilder.segmentDescriptionTitleExist()).to.be.true; 
+    })
+    it('segmentDescriptionInputExist', async () => {
+      expect(await segmentBuilder.segmentDescriptionInputExist()).to.be.true; 
+    })
+    it('segmentCreatedDateExist', async () => {
+      expect(await segmentBuilder.segmentCreatedDateExist()).to.be.true; 
+    })
+    it('dropAttributeHereFieldExist', async () => {
+      expect(await segmentBuilder.dropAttributeHereFieldExist()).to.be.true; 
+    })
+    it('droppableSectionArrowExist', async () => {
+      expect(await segmentBuilder.droppableSectionArrowExist()).to.be.true; 
+    })
+    it('cancellButtonExist', async () => {
+      expect(await segmentBuilder.cancelButtonExist()).to.be.true; 
+    })
+    it('saveButtonExist', async () => {
+      expect(await segmentBuilder.saveButtonExist()).to.be.true; 
+    })
+   })
+
+  context("categoryPart elements", async() => { 
+    it('categoryMySegmentsExist', async () => {
+      expect(await segmentBuilder.categoryMySegmentsExist()).to.be.true; 
+    })
+    it('categoryMySegmentsImageExist', async () => {
+      expect(await segmentBuilder.categoryMySegmentsImageExist()).to.be.true; 
+    })
+    it('categoryMySegmentsAngleRightExist', async () => {
+      expect(await segmentBuilder.categoryMySegmentsAngleRightExist()).to.be.true; 
+    })    
+    it('categoryMySegmentsIcon1Exist', async () => {
+      expect(await segmentBuilder.categoryMySegmentsIcon1Exist()).to.be.true; 
+    })
+    it('categoryMySegmentsIcon2Exist', async () => {
+      expect(await segmentBuilder.categoryMySegmentsIcon2Exist()).to.be.true; 
+    })
+    it('categoryMySegmentsNameExist', async () => {
+      expect(await segmentBuilder.categoryMySegmentsNameExist()).to.be.true; 
+    })
+  })
+
+  // it('dropAttributeHereFieldExist', async () => {
+  //   expect(await segmentBuilder.dropAttributeHereFieldExist()).to.be.true; 
   // })
-  // it('addSegmentButtonExist', async () => {
-  //   expect(await segmentBuilder.addSegmentButtonExist()).to.be.true; 
-  // })
-  
 
 })
 
